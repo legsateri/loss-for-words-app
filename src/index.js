80600d64db5e8eb30e9a99106a0768132c08e0d4
Allegra Pusateri <allegrapusateri@gmail.com>
2019-09-29 20:57:46 -0500

    troubleshoot scroll issue

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import ScrollToTop from './ScrollToTop/ScrollToTop';
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <ScrollToTop>
+            <App />
+        </ScrollToTop>
     </BrowserRouter>,
     document.getElementById('root')
 );
