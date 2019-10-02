f55cd15a1c2350cc5221bb54e59b407e3120242f
Allegra Pusateri <allegrapusateri@gmail.com>
2019-10-01 23:23:05 -0500

    fixed fonts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import ScrollToTop from './ScrollToTop/ScrollToTop';
 
 ReactDOM.render(
     <BrowserRouter>
-        <ScrollToTop>
-            <App />
-        </ScrollToTop>
+        <App />
     </BrowserRouter>,
     document.getElementById('root')
 );
