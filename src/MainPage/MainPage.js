9f32b1f8b17624b2d02fa39c536ed22f5ac8cd22
Allegra Pusateri <allegrapusateri@gmail.com>
2019-10-09 20:03:10 -0500

    fixed typo

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -39,7 +39,7 @@ class MainPage extends Component {
         </section>
 
         <section className='site_description'>
-          <p className='para_one'> Got writer's block? Get the creative juices flowing with a writing prompt or two. Some are rude, some a crude, but all our prompts have just the right amount of 'tude to get you thinking outside the box.</p>
+          <p className='para_one'> Got writer's block? Get the creative juices flowing with a writing prompt or two. Some are rude, some are crude, but all our prompts have just the right amount of 'tude to get you thinking outside the box.</p>
           <p className='para_two'>And if at the end of the day you really like what you had to say, share your words with your peers—you might just have the beginnings of a new story on your hands.</p>
         </section>
       </>
